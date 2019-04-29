d93f44ea898c629264bb2548c35364011b0c7c36
riveraes <Esteban.Rivera@teradyne.com>
2019-04-29 07:01:16 -0600

    UnitTest - Cambio de Clave - Agregue Test Case PruebaCambioClave10_ClaveIncorrecta3Veces()

--- UnitTest/CambioClave/CodedUITest1.cs
+++ UnitTest/CambioClave/CodedUITest1.cs
@@ -41,7 +41,7 @@ namespace UnitTest
         [TestCleanup]
         public void CloseApp()
         {
-            myProcess.Close();
+            myProcess.CloseMainWindow();
             System.Threading.Thread.Sleep(2000);
         }
 
@@ -283,6 +283,34 @@ namespace UnitTest
 
             // Verificar indicadores en el LCD de alarma y armado
             this.UIMap.Rivera_AssertLcdArmadoOn();
+            this.UIMap.Rivera_AssertLcdAlarmaOn();
+
+            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
+        }
+
+        [TestMethod]
+        public void PruebaCambioClave10_ClaveIncorrecta3Veces()
+        {
+            // Verificar que no hay alarma
+            this.UIMap.Rivera_AssertLcdAlarmaOff();
+
+            // Fallar la clave 3 veces
+            // Cambio de Clave
+            this.UIMap.Rivera_CambioClave_DigitarClavePorDefecto();
+            // Fallo 1
+            this.UIMap.Rivera_DigitarClaveModificada();
+            this.UIMap.Rivera_DigitarClavePorDefecto();
+            // Fallo 2
+            this.UIMap.Rivera_DigitarClaveModificada();
+            this.UIMap.Rivera_DigitarClavePorDefecto();
+            // Fallo 3
+            this.UIMap.Rivera_DigitarClaveModificada();
+            this.UIMap.Rivera_DigitarClavePorDefecto();
+            // Fallo 4
+            this.UIMap.Rivera_DigitarClaveModificada();
+            this.UIMap.Rivera_DigitarClavePorDefecto();
+
+            // Verificar que se activo alarma
             this.UIMap.Rivera_AssertLcdAlarmaOn();
 
             // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
